54ab04c56ff734398e26ff6ba10cd9b71c092d6d
yannsecundo <yannsecundo34@gmail.com>
2022-07-20 04:17:16 -0400

    requisito 2, observação alguns tests passam puxando o item do html direto pro expect, se colocar numa const não funciona

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,59 @@
-test('', () => {});
-// mexendo no test
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando a Page About', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+    userEvent.click(linkAbout);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/about');
+    const aboutTitle = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    userEvent.click(linkAbout);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/about');
+
+    const text1 = screen.getByText(
+      /this application simulates a pokédex, a digital encyclopedia containing all/i,
+    );
+    expect(text1).toBeInTheDocument();
+
+    const text2 = screen.getByText(
+      /one can filter pokémons by type, and see more details for each one of them/i,
+    );
+    expect(text2).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+    userEvent.click(linkAbout);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/about');
+
+    expect(
+      screen.getByRole('img', {
+        name: 'Pokédex',
+      }).src,
+    ).toBe(
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});

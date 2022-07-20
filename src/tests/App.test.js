1a09612649a7bb431bf9553f6707c7a66a43bc65
yannsecundo <yannsecundo34@gmail.com>
2022-07-20 02:37:49 -0400

    testando o primeiro requisito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Validando os links da pagina inicial', () => {
+  it('Testando o link de holme', () => {
+    renderWithRouter(<App />);
+    const textLinkHome = screen.getByRole('link', { name: 'Home' });
+    expect(textLinkHome).toBeInTheDocument();
+  });
+  it('Testando o link de Aboult', () => {
+    renderWithRouter(<App />);
+    const textLinkAbout = screen.getByRole('link', { name: 'About' });
+    expect(textLinkAbout).toBeInTheDocument();
+  });
+  it('Testando o link de Pokemon Favorito', () => {
+    renderWithRouter(<App />);
+    const textLinkFavPoke = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(textLinkFavPoke).toBeInTheDocument();
+  });
+});
